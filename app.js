2902e931813cc1c667bc4ecac1b0016a63749b12
bunnysocks <hemansakkthivelms14@gmail.com>
2026-01-09 10:12:33 +0530

    parsed tracker got tracker url

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-import fs from 'fs'
-import e from 'express'
-const app = e()
-const PORT = 3000
+import fs from "fs";
+import bencode from "bencode";
 
-const readTorrent = async () => {
-    try {
-        return fs.readFileSync('puppy.torrent', 'utf8')
-    } catch (err) {
-        console.log(err)
-    }
-}
+const data = fs.readFileSync("[CuaP] One Piece - 0001-0050 [1080p][HEVC 10bits].torrent");
+const decoded = bencode.decode(data);
 
-app.get("/", async (_,res) => {
-    const data = await readTorrent()
-    res.send(data)
-})
-
-app.listen(PORT, () => {
-    console.log(`server is listening on http://127.0.0.1:${PORT}`)
-})
+console.log(decoded);
